cf3c6cdea43d60451bdcd2da14ba9f6b13e34fc7
codeddave <drizzzydave@gmail.com>
2021-12-29 13:55:30 +0100

    updated icons in search bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import SearchIcon from "../../assets/icons/search.svg";
+import LocationIcon from "../../assets/icons/location.svg";
+
 import "./Header.css";
 //import {Navbar} from "../../components"
 const Header = () => {
@@ -14,16 +17,18 @@ const Header = () => {
         </section>
       </div>
 
-      <section className="w-4/5 mx-auto -m-6 bg-white h-16 border rounded px-4  shadow-lg">
+      <section className="w-4/5 mx-auto -m-6 bg-white h-16 border rounded px-6  shadow-lg">
         <div className="shaunded grid grid-cols-2 divide-x divide-cyan-500 my-2.5 ">
-          <div className="py-2">
+          <div className="py-2 flex items-center">
+            <img src={SearchIcon} alt="" className="pr-4" />
             <input
-              className="focus:outline-none text-black"
+              className="focus:outline-none text-black "
               type="text"
               placeholder="Front end developer"
             />
           </div>
-          <div className="pl-6 py-2">
+          <div className="pl-6 py-2 flex items-center">
+            <img src={LocationIcon} alt="" className="pr-4" />
             <input
               className="outline-none text-black"
               type="text"
