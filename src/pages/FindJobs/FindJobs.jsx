e9f46f859b496be934496d3aab524cf92ca8e3a0
codeddave <drizzzydave@gmail.com>
2021-12-29 16:01:27 +0100

    updates to Job Card component

--- src/pages/FindJobs/FindJobs.jsx
+++ src/pages/FindJobs/FindJobs.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import JobCard from "../../components/JobCard/JobCard";
 
 const FindJobs = () => {
   return (
     <div className="text-white">
       <Header />
+      <section className="grid grid-cols-2 w-4/5 mx-auto pt-">
+        <div>
+          {[0, 1, 2, 3, 4, 5].map((item) => (
+            <JobCard />
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
