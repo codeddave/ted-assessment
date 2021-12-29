d2598955f45a499d5e648105b2623dabcc31d017
codeddave <drizzzydave@gmail.com>
2021-12-29 20:06:14 +0100

    added JobDetails component to Find jobs page

--- src/pages/FindJobs/FindJobs.jsx
+++ src/pages/FindJobs/FindJobs.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import JobCard from "../../components/JobCard/JobCard";
+import JobDetails from "../../components/JobDetails/JobDetails";
 
 const FindJobs = () => {
   return (
     <div className="text-white">
       <Header />
-      <section className="grid grid-cols-2 w-4/5 mx-auto pt-">
+      <section className="grid grid-cols-2 gap-x-6 w-4/5 mx-auto pt-20">
         <div>
-          {[0, 1, 2, 3, 4, 5].map((item) => (
+          {[0, 1, 2, 3].map((item) => (
             <JobCard />
           ))}
+        </div>
+        <div>
+          <JobDetails />
         </div>
       </section>
     </div>
