91a940b8c744f9149b4de2b4c01c17634e1ad2ea
codeddave <drizzzydave@gmail.com>
2021-12-30 14:42:47 +0100

    modal styling update

--- src/components/JobApplicationForm/JobApplicationForm.jsx
+++ src/components/JobApplicationForm/JobApplicationForm.jsx
@@ -1,10 +1,29 @@
-import { Form, Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 import React from "react";
+import CustomFormInput from "../CustomFormInput.jsx/CustomFormInput";
 
 const JobApplicationForm = () => {
   return (
-    <section>
-      <Formik>{() => <Form></Form>}</Formik>
+    <section
+      style={{
+        height: "65vh",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+      className=" in bg-black shadow-xl p-6 transform transition-all w-full md:max-w-2xl opacity-100 overflow-x-hidden overflow-y-auto fixed z-50"
+    >
+      <Formik>
+        {() => (
+          <Form>
+            <Field
+              name="first_name"
+              title="First Name"
+              component={CustomFormInput}
+            />
+          </Form>
+        )}
+      </Formik>
     </section>
   );
 };
