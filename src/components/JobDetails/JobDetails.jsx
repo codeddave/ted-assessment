1d4fbf44a22407e5f58c9f5edf435a818a79b3dd
codeddave <drizzzydave@gmail.com>
2021-12-30 07:34:25 +0100

    styling updates to Job Details page

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -3,11 +3,9 @@ import LocationIcon from "../../assets/icons/location.svg";
 
 const JobDetails = () => {
   return (
-    <section className="border shadow-lg rounded-lg h-full bg-white text-gray-700">
+    <section className="border shadow-lg rounded-lg h-full bg-white text-gray-700 tracking-widest">
       <div className=" py-6 px-8 ">
-        <p className="text-2xl tracking-widest company-blue-text">
-          Front end developer
-        </p>
+        <p className="text-2xl  company-blue-text">Front end developer</p>
         <div className="flex pt-4 items-center">
           <img src={LocationIcon} alt="location" className="" />
 
@@ -27,41 +25,41 @@ const JobDetails = () => {
             responsible for profiling and improving front-end performance and
             documenting our front-end codebase.
           </p>
-          <ul className="list-disc">
+          <ul className="list-disc pt-6 pl-6">
             <li>Minimum Qualification: Degree</li>
             <li>Experience Level: Senior Level</li>
             <li>Experience Length: 5 years</li>
           </ul>
-          <section>
+          <section className="pt-7">
             <p>Job Description/ Requirements</p>
-            <ul className="list-disc">
-              <li>
+            <ul className="list-disc pt-7 pl-6">
+              <li className="mb-6">
                 5+ years experience of front-end related (HTML5 + JS + CSS3)
                 development work experience, familiar with mobile application
                 development;
               </li>
-              <li>
+              <li className="mb-6">
                 Proficient in JavaScript / Typescript, mastering technologies
                 such as HTML, CSS, DOM, AJAX, etc., can quickly complete the
                 established interactive design functions; At least 4 years
                 experience is required.
               </li>
-              <li>
+              <li className="mb-6">
                 Thorough understanding of React.js and its core principles
               </li>
-              <li>
+              <li className="mb-6">
                 Experience with popular React.js workflows (such as Flux or
                 Redux)
               </li>
-              <li>
+              <li className="mb-6">
                 Experience with popular React.js workflows (such as Flux or
                 Redux)
               </li>
-              <li>Knowledge of isomorphic react is a plus</li>
-              <li>
+              <li className="mb-6">Knowledge of isomorphic react is a plus</li>
+              <li className="mb-6">
                 Familiarity with mordern front-end build pipelines and tools
               </li>
-              <li>
+              <li className="mb-6">
                 Experience with common front-end development tools such as
                 Babel, Webpack, NPM e.t.c
               </li>
