686e38887a69470931f1f20071702d35ca1e42f6
codeddave <drizzzydave@gmail.com>
2021-12-30 07:35:00 +0100

    include footer

--- src/pages/FindJobs/FindJobs.jsx
+++ src/pages/FindJobs/FindJobs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import JobCard from "../../components/JobCard/JobCard";
 import JobDetails from "../../components/JobDetails/JobDetails";
@@ -7,7 +8,7 @@ const FindJobs = () => {
   return (
     <div className="text-white">
       <Header />
-      <section className="grid grid-cols-2 gap-x-6 w-4/5 mx-auto pt-20">
+      <section className="grid grid-cols-2 gap-x-6  w-4/5 mx-auto pt-20">
         <div>
           {[0, 1, 2, 3].map((item) => (
             <JobCard />
@@ -17,6 +18,7 @@ const FindJobs = () => {
           <JobDetails />
         </div>
       </section>
+      <Footer />
     </div>
   );
 };
