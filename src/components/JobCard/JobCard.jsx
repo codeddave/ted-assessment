18307c4635a0120589873f82764af6f6f67811e2
codeddave <drizzzydave@gmail.com>
2021-12-30 23:32:42 +0100

    updated Job Card

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -3,20 +3,26 @@ import LocationIcon from "../../assets/icons/location.svg";
 
 const JobCard = () => {
   return (
-    <article className="company-blue-bg shadow-lg border p-4 mb-6 rounded-lg">
+    <article className="company-blue-bg shadow-lg border p-4 mb-6 rounded-lg tracking-wider">
       <div className="flex justify-between">
         <p className="tracking-wider">Front end developer</p>
-        <p>$2k-$3k</p>
+        <p className="">$2k-$3k</p>
       </div>
       <div className="flex pt-4">
         <img src={LocationIcon} alt="location" />
 
-        <p className="pl-3">Ikeja, Lagos</p>
+        <p className="pl-3 font-light">Ikeja, Lagos</p>
       </div>
       <p className="text-sm pt-4">
-        We seek an experienced Javascript web application developer who is
-        proficient with React.js and has excellent communication skills.
+        The ideal candidate should be comfortable with the following technology
+        stacks: HTML, CSS, JavaScript & Vue.js and/or React.js and everything in
+        between.
       </p>
+      <div className="flex justify-end">
+        <button className="py-2 px-4 border rounded-xl mt-3 mb-2 tracking-wider company-red-text bg-white">
+          See more
+        </button>
+      </div>
     </article>
   );
 };
