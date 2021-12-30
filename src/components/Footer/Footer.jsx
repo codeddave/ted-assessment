c7b047ac5b15a76ac8668ea3acfc35256d6bae6e
codeddave <drizzzydave@gmail.com>
2021-12-30 11:20:22 +0100

    updated social media icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Logo from "../../assets/icons/logo.svg";
+import Instagram from "../../assets/icons/instagram.svg";
+import Facebook from "../../assets/icons/facebook.svg";
+import Twitter from "../../assets/icons/twitter.svg";
 
 const Footer = () => {
   return (
-    <footer className="company-blue-bg py-24 ">
-      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 justify-between mx-auto w-4/5 bordr">
+    <footer className="company-blue-bg py-24 relati ">
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 justify-between mx-auto w-4/5 bordr relat">
         <div>
           <img src={Logo} alt="logo" />
 
@@ -30,9 +33,34 @@ const Footer = () => {
             <li>Terms and condition</li>
           </ul>
         </div>
-        <div>
+        <div className="flex justify-cent items-center flex-col">
           <p className="text-2xl">Social Media</p>
-          <div className="pt-4"> Icons go here</div>
+          <div className="pt-1.5 flex">
+            <div className="relative mr-5">
+              <div className="w-12 h-12 rounded-full flex items-center  bg-gray-100 opacity-25 relaive z-30   justify-center"></div>
+              <img
+                src={Instagram}
+                alt="instagram"
+                className="absolute  top-4 right-4 "
+              />
+            </div>
+            <div className="relative mr-5">
+              <div className="w-12 h-12 rounded-full flex items-center  bg-gray-100 opacity-25 relaive z-30   justify-center"></div>
+              <img
+                src={Facebook}
+                alt="instagram"
+                className="absolute top-3.5 right-5"
+              />
+            </div>
+            <div className="relative ">
+              <div className="w-12 h-12 rounded-full flex items-center  bg-gray-100 opacity-25 relaive z-30   justify-center"></div>
+              <img
+                src={Twitter}
+                alt="instagram"
+                className="absolute top-4 right-4"
+              />
+            </div>
+          </div>
         </div>
       </section>
     </footer>
