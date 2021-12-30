69f3e2428217893d8239b1336b1c35c142ea59d1
codeddave <drizzzydave@gmail.com>
2021-12-30 07:33:38 +0100

    added ellipses to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,22 +2,45 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import SearchIcon from "../../assets/icons/search.svg";
 import LocationIcon from "../../assets/icons/location.svg";
+import EllipseOne from "../../assets/images/ellipse1.svg";
+import EllipseTwo from "../../assets/images/ellipse2.svg";
+import EllipseThree from "../../assets/images/ellipse3.svg";
 
 import "./Header.css";
 //import {Navbar} from "../../components"
 const Header = () => {
   return (
     <>
-      <div className="0 text-white h-56 header-bg">
-        <section className="w-5/6 mx-auto">
-          <Navbar />
-          <section className="pt-20">
+      <div className="0 text-white h-72 header-bg relative">
+        <section className="w-5/6 mx-auto ">
+          <Navbar />{" "}
+          <section className="absolute right-0">
+            <img
+              src={EllipseOne}
+              alt=""
+              className=""
+              //style={{ width: "50px", height: "50px" }}
+            />
+            <img
+              src={EllipseTwo}
+              alt=""
+              className="-ml-8 pt-2"
+              //style={{ width: "50px", height: "50px" }}
+            />
+            <img
+              src={EllipseThree}
+              alt=""
+              className="-ml-24 pt-2"
+              //style={{ width: "50px", height: "50px" }}
+            />
+          </section>
+          <section className="pt-20 ">
             <p className="text-4xl tracking-widest pl-8">Find Your Dream Job</p>
           </section>
         </section>
       </div>
 
-      <section className="w-4/5 mx-auto -m-6 bg-white h-16 border rounded px-6  shadow-lg">
+      <section className="w-4/5 mx-auto -mt-6 z-20 relative bg-white h-16 border rounded px-6  shadow-lg">
         <div className="shaunded grid grid-cols-2 divide-x divide-cyan-500 my-2.5 ">
           <div className="py-2 flex items-center">
             <img src={SearchIcon} alt="" className="pr-4" />
