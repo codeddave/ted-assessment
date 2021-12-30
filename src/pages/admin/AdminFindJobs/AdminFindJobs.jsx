9888249bdf54e189356bed8191d055c9eb82ef9e
codeddave <drizzzydave@gmail.com>
2021-12-30 22:19:26 +0100

    updated styling so Footer sticks to bottom

---
+++ src/pages/admin/AdminFindJobs/AdminFindJobs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AdminFooter from "../../../components/admin/AdminFooter/AdminFooter";
+import AdminHeader from "../../../components/admin/AdminHeader/AdminHeader";
+const AdminFindJobs = () => {
+  return (
+    <div className="flex flex-col h-full overflow-hidden min-h-screen">
+      <AdminHeader />
+      <AdminFooter />
+    </div>
+  );
+};
+
+export default AdminFindJobs;
