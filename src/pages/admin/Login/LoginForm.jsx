c664759921b0ab69ea36349defe41e84423cac61
codeddave <drizzzydave@gmail.com>
2021-12-31 19:34:49 +0100

    soft syling updates

--- src/pages/admin/Login/LoginForm.jsx
+++ src/pages/admin/Login/LoginForm.jsx
@@ -4,17 +4,25 @@ import CustomFormInput from "../../../components/CustomFormInput.jsx/CustomFormI
 const LoginForm = () => {
   return (
     <div>
-      <Formik>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+      >
         {() => (
-          <Form className="dead-center border border-black w-3/4">
+          <Form className="dead-center ack w-3/4">
+            <p className="company-blue-text text-4xl  tracking-wider font-extrabold pb-11">
+              Login
+            </p>
             <Field name="email" title="Email" component={CustomFormInput} />
             <Field
               name="password"
               title="Password"
               component={CustomFormInput}
             />{" "}
-            <button className="w-full my-11  company-blue-bg text-white py-6 rou">
-              Submit Application
+            <button className="w-40 mt-10  company-blue-bg text-white h-16 rounded-lg">
+              Login
             </button>
           </Form>
         )}
