85de4e9917d91eb637432255c268646b70598745
codeddave <drizzzydave@gmail.com>
2021-12-31 11:56:34 +0100

    created jobRow component

---
+++ src/components/admin/JobRow/JobRow.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const JobRow = () => {
+  return (
+    <section className="pl-8 pr-12 shadow-lg bg-white flex h-16 mb-3 w-4/5 mx-auto justify-between rounded-lg items-center text-gray-500">
+      <div>
+        <p>Front end developer</p>
+      </div>
+
+      <p>12/7/21</p>
+      <p>50</p>
+      <p>Edit</p>
+      <p>Delete</p>
+    </section>
+  );
+};
+
+export default JobRow;
