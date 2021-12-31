a126d5a2d319c80dce46c34268d785ccd33055d6
codeddave <drizzzydave@gmail.com>
2021-12-31 16:10:32 +0100

    updates to Job Row component, added red dot

--- src/components/admin/JobRow/JobRow.jsx
+++ src/components/admin/JobRow/JobRow.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import RedDot from "../../../assets/icons/red-dot.svg";
 
 const JobRow = () => {
   return (
-    <section className="pl-8 pr-12 shadow-lg bg-white flex h-16 mb-3 w-4/5 mx-auto justify-between rounded-lg items-center text-gray-500">
-      <div>
+    <section className="pl-6 pr-12 shadow-lg bg-white flex h-16 mb-3 w-4/5 mx-auto justify-between rounded-lg items-center text-gray-500">
+      <div className="flex">
+        <img src={RedDot} alt="" className="pr-4" />
         <p>Front end developer</p>
       </div>
 
