a6be67098744402bc8d8b76d2ea2cae64fa50e3f
codeddave <drizzzydave@gmail.com>
2021-12-31 13:01:25 +0100

    Job creation form

---
+++ src/components/admin/JobCreationForm/JobCreationForm.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Field, Form, Formik } from "formik";
+import CloseIcon from "../../../assets/icons/close.svg";
+import CustomFormInput from "../../CustomFormInput.jsx/CustomFormInput";
+
+const JobCreationForm = ({ closeModal }) => {
+  return (
+    <section
+      style={{
+        height: "65vh",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+      className="modal-bg-color shadow-xl p-6 transform transition-all w-full md:max-w-2xl opacity-100 overflow-x-hidden overflow-y-scroll fixed z-50 tracking-wider company-blue-text scroll "
+    >
+      <img
+        onClick={closeModal}
+        src={CloseIcon}
+        className="right-6 top-8 absolute"
+        alt=""
+      />
+      <p className="text-2xl mt-6">New Job</p>
+      <div className="flex pt-5">
+        <p className="font-light pl-2.5">
+          Kindly provide the required information to get matched with suitable
+          candidates
+        </p>
+      </div>
+      <Formik>
+        {() => (
+          <Form className="pt-10">
+            <Field
+              name="first_name"
+              title="First Name"
+              component={CustomFormInput}
+            />
+            <Field
+              name="last_name"
+              title="Last Name"
+              component={CustomFormInput}
+            />{" "}
+            <Field
+              name="email"
+              title="Email Address"
+              component={CustomFormInput}
+            />
+            <Field
+              name="location"
+              title="Location"
+              component={CustomFormInput}
+            />
+            <Field
+              name="phone"
+              title="Phone Number"
+              component={CustomFormInput}
+            />
+            <button className="w-full my-11  company-blue-bg text-white py-6 rou">
+              Submit Application
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </section>
+  );
+};
+
+export default JobCreationForm;
