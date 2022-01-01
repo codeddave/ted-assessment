5aa3c90d39726f6e9de03e062a77b460e1c76647
codeddave <drizzzydave@gmail.com>
2022-01-01 03:12:08 +0100

    testing smn

--- src/api/admin/index.js
+++ src/api/admin/index.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 export const logIn = async (loginData) => {
   try {
-    await axios.post(`https://api.jobboard.tedbree.com/v1/login`, loginData);
+    await axios.post(
+      `https://peaceful-caverns-36182.herokuapp.com/https://api.jobboard.tedbree.com/v1/login`,
+      loginData
+    );
   } catch (error) {
     console.log(error);
   }
