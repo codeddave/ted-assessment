27fa62f8d28f00640767bec8c22d2c8f17a33b88
codeddave <drizzzydave@gmail.com>
2022-01-03 10:46:06 +0100

    updates to log in fn

--- src/api/admin/index.js
+++ src/api/admin/index.js
@@ -1,10 +1,17 @@
 import axios from "axios";
 
 export const logIn = async (loginData) => {
+  let config = {
+    headers: {
+      "Content-Type": "application/json",
+      "X-Requested-With": "XMLHttpRequest",
+    },
+  };
   try {
     await axios.post(
       `https://peaceful-caverns-36182.herokuapp.com/https://api.jobboard.tedbree.com/v1/login`,
-      loginData
+      loginData,
+      config
     );
   } catch (error) {
     console.log(error);
